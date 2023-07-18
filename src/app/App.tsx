0c50de273f2386a254b6b26d2c38b50ae96b8686
ShineWxrk <ShineWxrk@gmail.com>
2023-07-18 14:05:41 +0300

    add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
+import { AppRouter } from './providers/router'
 
 
 
@@ -19,12 +20,7 @@ const App = () => {
       <button onClick={toogleTheme}>TOOGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />}></Route>
-          <Route path={'/'} element={<MainPage />}></Route>
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div> 
   )
 }
