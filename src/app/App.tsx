6c89fc764d99562c2c6a238b223e7de3bb2f9aea
ShineWxrk <ShineWxrk@gmail.com>
2023-07-18 14:48:52 +0300

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ import { useTheme } from './providers/ThemeProvider'
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 
 
@@ -18,8 +19,8 @@ const App = () => {
   return (
     <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
       <button onClick={toogleTheme}>TOOGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      
+      <Navbar />
       <AppRouter />
     </div> 
   )
