3ed892f94c3cce1df1b2c1190e2e9f1254090846
ShineWxrk <ShineWxrk@gmail.com>
2023-07-20 23:18:28 +0300

    add loader, not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,21 +1,20 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import React, { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader'
 
 const AppRouter = () => {
   return (
-    <div className='page-wrapper'>
-      <Suspense fallback={<div>Loading...</div>}>
+    <div className="page-wrapper">
+      <Suspense fallback={<PageLoader />}>
         <Routes>
-          {Object.values(routeConfig).map(({element, path}) => (
+          {Object.values(routeConfig).map(({ element, path }) => (
             <Route key={path} path={path} element={element} />
           ))}
         </Routes>
       </Suspense>
     </div>
-  );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
