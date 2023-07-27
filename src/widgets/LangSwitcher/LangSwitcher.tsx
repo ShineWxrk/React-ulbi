01b64a9a27fff0f0a3ca895a95f6852338087deb
ShineWxrk <ShineWxrk@gmail.com>
2023-07-27 20:18:38 +0300

    update sidebar

--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -1,28 +1,29 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './LangSwitcher.module.scss'
-import { useTranslation } from 'react-i18next';
-import Button, { ThemeButton } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next'
+import Button, { ButtonTheme } from 'shared/ui/Button/Button'
 
 interface LangSwitcherProps {
-  className?: string,
+  className?: string
+  short?: boolean
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
   const { t, i18n } = useTranslation()
-  
+
   const toggle = () => {
     i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
   }
 
   return (
-      <Button 
-        className={classNames(cls.LangSwitcher, {}, [className])}
-        theme={ThemeButton.CLEAR} 
-        onClick={toggle}
-      >
-        {t('Язык')}
-      </Button>
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={ButtonTheme.CLEAR}
+      onClick={toggle}
+    >
+      {t(short ? 'Короткий язык' : 'Язык')}
+    </Button>
   )
-};
+}
 
-export default LangSwitcher;
+export default LangSwitcher
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,30 +1,72 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Sidebar.module.scss'
-import { useState } from 'react';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher';
-import { useTranslation } from 'react-i18next';
+import { useState } from 'react'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher'
+import { useTranslation } from 'react-i18next'
+import Button, { ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
+import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { RoutePath } from 'shared/config/routeConfig/routeConfig'
+
+import MainIcon from 'shared/assets/icons/main-20-20.svg'
+import AboutIcon from 'shared/assets/icons/about-20-20.svg'
 
 interface SidebarProps {
-  className?: string,
+  className?: string
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
+export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false)
-  const {t} = useTranslation()
+  const { t } = useTranslation()
 
   const onToggle = () => {
-    setCollapsed(prev => !prev)
+    setCollapsed((prev) => !prev)
   }
   return (
-    <div data-testid='sidebar' className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
-      <button data-testid='sidebar-toggle' onClick={onToggle}>{t('Переключить')}</button>
+    <div
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <Button
+        data-testid="sidebar-toggle"
+        onClick={onToggle}
+        className={cls.collapseBtn}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        square
+        size={ButtonSize.L}
+      >
+        {collapsed ? '>' : '<'}
+      </Button>
+      <div className={cls.items}>
+        <div>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.main}
+            className={cls.item}
+          >
+            <MainIcon className={cls.icon} />
+            <span className={cls.link}>{t('Главная')}</span>
+          </AppLink>
+        </div>
+        <div>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.about}
+            className={cls.item}
+          >
+            <AboutIcon className={cls.icon} />
+            <span className={cls.link}>{t('О сайте')}</span>
+          </AppLink>
+        </div>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang}/>
+        <LangSwitcher className={cls.lang} short={collapsed} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
